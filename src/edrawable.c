0a61eee282d4fcb540d7d532928992a22ecdead9
IPlinux build user <build@frog.(none)>
2009-03-17 11:21:02 +0000

    All API required to toys

--- src/edrawable.c
+++ src/edrawable.c
@@ -6,7 +6,7 @@ Ewl_Drawable *
 ewl_drawable_init(Ewl_Drawable *e);
 
 EAPI Ewl_Drawable *
-ewl_drawablewl_new() {
+ewl_drawable_new() {
     Ewl_Drawable *drawable;
 
     DENTER_FUNCTION(DLEVEL_STABLE);
@@ -30,6 +30,7 @@ ewl_drawable_configure(Ewl_Drawable *e) {
     Drawable_Image di;
 
     image = EWL_IMAGE(e)->image;
+    evas_object_image_filled_set(image, 1);
     evas_object_image_size_get(image, &w, &h);
     data = evas_object_image_data_get(image, 1);
 
@@ -37,6 +38,11 @@ ewl_drawable_configure(Ewl_Drawable *e) {
     drawable_context_set_image(e->context, di);
 }
 
+
+static void
+ewl_drawable_cb_configure(Ewl_Widget *w, void *ev_data, void *user_data) {
+    ewl_drawable_configure(EWL_DRAWABLE(w));
+}
 
 static void
 ewl_drawable_cb_destroy(Ewl_Widget *w, void *ev_data /*__UNUSED__ */,
@@ -66,6 +72,8 @@ ewl_drawable_init(Ewl_Drawable *e){
         return NULL;
     ewl_callback_prepend(EWL_WIDGET(e),
             EWL_CALLBACK_DESTROY, ewl_drawable_cb_destroy, NULL);
+    ewl_callback_append(EWL_WIDGET(e), EWL_CALLBACK_CONFIGURE,
+            ewl_drawable_cb_configure, NULL);
     e->context = drawable_context_new();
     if(!e->context)
         return NULL;
@@ -74,8 +82,19 @@ ewl_drawable_init(Ewl_Drawable *e){
     DRETURN_PTR(e, DLEVEL_STABLE);
 }
 
-EAPI void         ewl_drawable_draw_line(Ewl_Drawable *e, int x1, int y1, int x2, int y2) {
+EAPI void
+ewl_drawable_draw_line(Ewl_Drawable *e, int x1, int y1, int x2, int y2) {
     drawable_image_draw_line(e->context, x1, y1, x2, y2, 0);
+}
+
+EAPI void
+ewl_drawable_draw_rectangle(Ewl_Drawable *e, int x1, int y1, int x2, int y2) {
+    drawable_image_draw_rectangle(e->context, x1, y1, x2, y2);
+}
+
+EAPI void
+ewl_drawable_draw_rectangle_fill(Ewl_Drawable *e, int x1, int y1, int x2, int y2) {
+    drawable_image_fill_rectangle(e->context, x1, y1, x2, y2);
 }
 
 EAPI EDrawablePolygon  
@@ -104,15 +123,44 @@ ewl_drawable_polygon_add(EDrawablePolygon p, int x, int y) {
 }
 
 EAPI void
-ewl_drawable_draw_arc(Ewl_Drawable* e, int x, int y, int r) {
-    drawable_image_draw_ellipse(e->context, x, y, r, r);
+ewl_drawable_draw_ellipse(Ewl_Drawable* e, int x, int y, int r, int r2) {
+    drawable_image_draw_ellipse(e->context, x, y, r, r2);
 }
 
 EAPI void
-ewl_drawable_draw_arc_filled(Ewl_Drawable* e, int x, int y, int r) {
-    drawable_image_fill_ellipse(e->context, x, y, r, r);
+ewl_drawable_draw_ellipse_filled(Ewl_Drawable* e, int x, int y, int r, int r2) {
+    drawable_image_fill_ellipse(e->context, x, y, r, r2);
 }
 
 EAPI void 
 ewl_drawable_draw_text(Ewl_Drawable* e, int x, int y, int size, char *font, char*font2, char *text) {
+}
+
+EAPI void
+ewl_drawable_set_colors(Ewl_Drawable *e, Ewl_Color_Set *set) {
+    drawable_context_set_color(e->context, set->r, set->g, set->b, set->a);
+}
+
+EAPI void         ewl_drawable_set_clip(Ewl_Drawable *e, int x, int y, int w, int h) {
+    drawable_context_set_cliprect(e->context, x, y, w, h);
+}
+
+EAPI void         ewl_drawable_reset_clip(Ewl_Drawable* e){
+    int w, h;
+    w = drawable_image_get_width(e->context);
+    h = drawable_image_get_height(e->context);
+    ewl_drawable_set_clip(e, 0, 0, w, h);
+}
+
+EAPI void
+ewl_drawable_commit(Ewl_Drawable *e) {
+    Ewl_Image *img = EWL_IMAGE(e);
+    void *in;
+    int w, h;
+
+    in = drawable_image_get_data(e->context);
+    w = drawable_image_get_width(e->context);
+    h = drawable_image_get_height(e->context);
+    evas_object_image_data_set(img->image, in);
+    evas_object_image_data_update_add(img->image, 0, 0, w, h);
 }
--- src/edrawable.h
+++ src/edrawable.h
@@ -20,15 +20,23 @@ EAPI void         ewl_drawable_destroy(Ewl_Drawable *);
 
 EAPI void         ewl_drawable_draw_line(Ewl_Drawable *, int x1, int y1, int x2, int y2);
 
+EAPI void         ewl_drawable_set_colors(Ewl_Drawable *, Ewl_Color_Set *);
+EAPI void         ewl_drawable_draw_rectangle(Ewl_Drawable *, int, int, int, int);
+EAPI void         ewl_drawable_draw_rectangle_fill(Ewl_Drawable *, int, int, int, int);
+
 EAPI EDrawablePolygon  ewl_drawable_polygon_new();
 EAPI void         ewl_drawable_draw_polygon(Ewl_Drawable *, EDrawablePolygon );
 EAPI void         ewl_drawable_draw_polygon_fill(Ewl_Drawable *, EDrawablePolygon );
 EAPI void         ewl_drawable_polygon_delete(EDrawablePolygon);
 EAPI void         ewl_drawable_polygon_add(EDrawablePolygon, int, int);
 
-EAPI void         ewl_drawable_draw_arc(Ewl_Drawable* , int x, int y, int r);
-EAPI void         ewl_drawable_draw_arc_filled(Ewl_Drawable*, int x, int y, int r);
+EAPI void         ewl_drawable_draw_ellipse(Ewl_Drawable* , int x, int y, int r, int r2);
+EAPI void         ewl_drawable_draw_ellipse_filled(Ewl_Drawable*, int x, int y, int r, int r2);
 
 EAPI void         ewl_drawable_draw_text(Ewl_Drawable*, int, int, int, char *, char*, char *);
 
+EAPI void         ewl_drawable_set_clip(Ewl_Drawable*, int, int, int, int);
+EAPI void         ewl_drawable_reset_clip(Ewl_Drawable*);
+
+EAPI void         ewl_drawable_commit(Ewl_Drawable *);
 #endif
